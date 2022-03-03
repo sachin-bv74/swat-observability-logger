ef70ab213e66bf257bbb5494be238520aab2a29c
VarshaAdigaBV <varsha.adiga@bazaarvoice.com>
2022-03-03 10:51:16 +0530

    winston logger

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,23 +1,4 @@
 
-
-// const log4js = require('log4js');
-
-// log4js.addLayout('json', config => function (logEvent) {
-//     return JSON.stringify(logEvent) + config.separator;
-//   });
-  
-//   log4js.configure({
-//     appenders: {
-//       out: { type: 'stdout', layout: { type: 'json', separator: ',' } },
-//       errorLogger: { type: 'file', filename: './logs/app.log', layout: { type: 'json', separator: ',' }}
-//     },
-//     categories: {
-//       default: { appenders: ['out', 'errorLogger'], level: 'error' },
-//     }
-//   });
-
-// const logger = log4js.getLogger('errorLogger');
-
 const { createLogger, format, transports } = require('winston');
 const { combine, timestamp, label, printf } = format;
 
